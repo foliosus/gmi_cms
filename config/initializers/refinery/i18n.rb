8923eff46718abc0008a936d92402bc0ebae11ab
Brent Miller <foliosus@foliosus.com>
2012-05-21 22:50:06 -0700

    Correct locale settings

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -11,7 +11,7 @@ Refinery::I18n.configure do |config|
 
   config.frontend_locales = [:en, :es]
   
-  config.locals = {:en => "English", :es => "Español"}
+  config.locales = {:en => "English", :es => "Español"}
   
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
 end
