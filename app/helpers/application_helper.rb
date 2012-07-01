2a6fdbc5cd5e989c422206655e2eb04a942d7b77
Brent Miller <foliosus@foliosus.com>
2012-07-01 11:41:17 -0700

    Convert to 1140 grid
    Also, fix up language switching links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def language_switching_link
+    new_lang = Globalize.locale == :en ? :es : :en
+    url = if request.original_fullpath.starts_with?("/#{Globalize.locale}")
+      request.original_fullpath.sub(/^\/#{Globalize.locale}/, "/#{new_lang}")
+    else
+      "/#{new_lang}#{request.original_fullpath}"
+    end
+    link_to Refinery::I18n.locales[new_lang], url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 GmiCms::Application.routes.draw do
-
-  # This line mounts Refinery's routes at the root of your application.
-  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
-  # If you would like to change where this extension is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
-  mount Refinery::Core::Engine, :at => '/'
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -63,4 +55,12 @@ GmiCms::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+  root :to => 'refinery/pages#home'
+  # This line mounts Refinery's routes at the root of your application.
+  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
+  # If you would like to change where this extension is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
+  mount Refinery::Core::Engine, :at => '/'
 end
